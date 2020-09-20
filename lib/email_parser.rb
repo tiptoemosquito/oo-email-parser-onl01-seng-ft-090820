34b5895a20a5888680fb3d7f068b60a1d57054c6
Christina Howard <clhoward33@icloud.com>
2020-09-20 19:08:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,8 +8,8 @@ class EmailAddressParser
     attr_accessor :emails
     def initialize(emails)
       @emails = emails
-    end 
-    def parse 
-      emails.split(", ").uniq 
-    end 
+    end
+    def parse
+      emails.split(", ").uniq
+    end
 end 
